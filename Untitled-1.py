fea7ccc0b6a18959467956702684b031a52b56c3
Artut Fattakhov <86257431+ArchFa@users.noreply.github.com>
2022-06-20 16:15:31 +0500

    add

--- Untitled-1.py
+++ Untitled-1.py
@@ -13,12 +13,17 @@ uploaded_file_who = "asd.csv"
 all = pd.read_csv(uploaded_file_one, sep='|')
 task = pd.read_csv(uploaded_file_who, sep='|')
 
-option = st.selectbox(
+col_multi, col_em = st.columns([1, 3])
+
+selected_sn = col_multi.selectbox(
     "Выберите промокод",
     options=all['state'].unique().tolist(),
     index=0,
 )
 
 
+
+df_ref_code = all[all['state']== selected_sn].duration.value_counts().to_frame()
+st.dataframe(df_ref_code)
 
 
