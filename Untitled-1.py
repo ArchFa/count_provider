7bb7702f6c10c9e978869478d8b48bfceb661973
Artut Fattakhov <86257431+ArchFa@users.noreply.github.com>
2022-06-20 16:04:21 +0500

    add

--- Untitled-1.py
+++ Untitled-1.py
@@ -6,15 +6,19 @@ import streamlit as st
 # %%
 st.title("Необходимое число провайдеров")
 
+uploaded_file_one = "main.csv"
+uploaded_file_who = "asd.csv"
 
-all = pd.read_csv('/Users/arturfattahov/Desktop/main.csv', sep='|')
-task = pd.read_csv('/Users/arturfattahov/Desktop/asd.csv', sep='|')
+
+all = pd.read_csv(uploaded_file_one, sep='|')
+task = pd.read_csv(uploaded_file_who, sep='|')
 
 option = st.selectbox.selectbox(
     "Выберите промокод",
     options=all['state'].unique().tolist(),
     index=0,
 )
+
 
 
 
