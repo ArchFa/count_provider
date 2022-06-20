72936b8e9ffad8b9eb9e66462807f7e65da1afb6
Artut Fattakhov <86257431+ArchFa@users.noreply.github.com>
2022-06-20 17:11:45 +0500

    add

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,6 +4,8 @@ import numpy as np
 import streamlit as st
 
 # %%
+st.set_page_config(layout="wide")
+
 st.title("Необходимое число провайдеров")
 
 uploaded_file_one = "main.csv"
@@ -14,13 +16,11 @@ all = pd.read_csv(uploaded_file_one, sep=',')
 task = pd.read_csv(uploaded_file_who, sep=',')
 
 
-st.write(all.head(5))
-st.write(task.head(5))
 
 col_multi, col_em = st.columns([2, 3])
 
 selected_sn = col_multi.selectbox(
-    "Выберите промокод",
+    "Выберите штат",
     options=all['state'].unique().tolist(),
     index=0,
 )
@@ -28,7 +28,7 @@ selected_sn = col_multi.selectbox(
 st.write(all.query('state == @selected_sn'))
 
 
-
+st.write("Суммарное еобходимое число провайдеров по штатам")
 
 # %%
 
