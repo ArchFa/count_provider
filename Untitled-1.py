dd78a95e72f5e916c09ccd0c8c8f3dc83155d014
Artut Fattakhov <86257431+ArchFa@users.noreply.github.com>
2022-06-21 17:17:00 +0500

    add

--- Untitled-1.py
+++ Untitled-1.py
@@ -17,12 +17,20 @@ task = pd.read_csv(uploaded_file_who, sep=',')
 zero_count = pd.read_csv(uploaded_file_third, sep=',')
 
 
-# all = all[['state','category_id' , 'offers_may', 'offers_no_responds_may', 
-# 'count_active_may', 'count_active_may_with_responds', 'offers_responds_may']]
+all = all[['state','category_id' , 'offers_may', 'offers_no_responds_may', 
+'count_active_may', 'count_active_may_with_responds', 'need', 'need_']]
 
-# all.columns = ['Штат', 'id категории', 'Количество задач', 
-#                 'Количество задач без отклика', 'Количество активных провайдеров',
-#                 'Количество активных провайдеров с откликом', 'Количество задач с откликом']
+all.columns = ['Штат', 'id категории', 'Количество задач', 
+                'Количество задач без отклика', 'Количество активных провайдеров',
+                'Количество активных провайдеров с откликом', 'Необходимое число провайдеров', 'Необходимое число провайдеров для поиска']
+
+task.columns = ['Штат', 'Необходимое число провайдеров', 'Необходимое число провайдеров для поиска']
+
+
+zero_count = zero_count[['state', 'category_id']]
+
+zero_count.columns = ['Штат', 'id_категории']
+
 
 
 st.title("Аналитика задач за май 2022")
@@ -33,11 +41,11 @@ st.subheader("Количество необходимых провайдеров
 col_multi, col_em = st.columns([2, 3])
 selected_sn = col_multi.selectbox(
     "Выберите штат",
-    options=all['state'].unique().tolist(),
+    options=all['Штат'].unique().tolist(),
     index=0,
 )
 
-st.write(all.query('state == @selected_sn'), width=300)
+st.write(all.query('Штат == @selected_sn'), width=300)
 
 
 st.write("""##### Суммарное необходимое количество провайдеров по штатам""")
@@ -50,16 +58,16 @@ st.subheader("Штаты с 0 задач и провайдеров")
 col_multi, col_em = st.columns([2, 3])
 selected_sn = col_multi.selectbox(
     "Выберите штат",
-    options=zero_count['state'].unique().tolist(),
+    options=zero_count['Штат'].unique().tolist(),
     index=0,
 )
 
 col_em.write("")
 col_em.write("")
 col_em.write(
-    f"{selected_sn} содержит {zero_count.query('state == @selected_sn').count()[0]} категорий без провайдеров и исполнителей"
+    f"{selected_sn} содержит {zero_count.query('Штат == @selected_sn').count()[0]} категорий без провайдеров и исполнителей"
 )
-st.write(zero_count.query('state == @selected_sn'), width=300)
+st.write(zero_count.query('Штат == @selected_sn'), width=300)
 
 ####################
 
