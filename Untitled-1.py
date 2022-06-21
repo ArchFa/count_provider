2f9a4867189c7087a4d49829f152b990b345b001
Artut Fattakhov <86257431+ArchFa@users.noreply.github.com>
2022-06-21 16:29:28 +0500

    add

--- Untitled-1.py
+++ Untitled-1.py
@@ -27,7 +27,7 @@ selected_sn = col_multi.selectbox(
 st.write(all.query('state == @selected_sn'), width=300)
 
 
-st.write("""### Суммарное необходимое количество провайдеров по штатам""")
+st.write("""#### Суммарное необходимое количество провайдеров по штатам""")
 
 st.write(task, width=200)
 
